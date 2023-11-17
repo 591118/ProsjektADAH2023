550239b11701dbc785dfe9672e451be554c13d31
MaggeSN <122047522+MaggeSN@users.noreply.github.com>
2023-11-17 16:01:55 +0100

    Realistiske paramtere

--- forbruksvaner1.py
+++ forbruksvaner1.py
@@ -6,14 +6,15 @@ import pandas as pd
 df = pd.read_csv("forbruksvaner.csv", delimiter=";", header=1, index_col=0)
 
 # Parameters
-alpha = 0.69     # Propensity to consume out of disposable income
-beta = 0.9      # Adjustment speed of spending habits
-#g = 0.02        # Growth rate of income (Må ha inflasjon i modell for å kunne bruke denne)
-U = 270000       # Fixed expenses 
+alpha = 0.95   # Propensity to consume out of disposable income
+beta = 0.8      # Adjustment speed of spending habits
+
+#g = 0.02        # Growth rate of income (Må ha inflasjon i modell for å kunne bruke denne. Antar i denne modellen at inflasjonen nuller ut økning i lønn.)
+U = 270000       # Fixed expenses (Gj. i norge i følge forbrukerundersøkelsen SSB)
 P = 96000       # Payment amount for loans (8k i mnd)
-r = 0.045        # Interest rate on loans (Dagens rentesats)
-L0 = 1622400     # Initial loan amount (338% av årslønnen som er gj. snitt ifølge Gjensidige)
-A0 = 480000     # Initial income (Gj. i Norge er rundt 53k)
+r = 0.03        # Interest rate on loans (Dagens rentesats) (Modellen antar fast rentesats)
+L0 = 1358700     # Initial loan amount (338% av årslønnen som er gj. snitt ifølge Gjensidige)
+A0 = 402000     # Initial income (Gj. nettolønn i Norge er 402 000kr)
 F0 = 100000      # Initial discretionary spending (Dette var gj. snitt i 1999 i følge SSB)
 
 # ODE system
@@ -33,12 +34,12 @@ F = odeint(dFdt, F0, t, args=(alpha, beta, U, P, r, L0, A0))
 print(df.loc[df.index[0]])
 print((df.loc[df.index[0]])-(df.loc[df.index[4]]))
 
-'''ny = []
-for element in df.loc[df.index[0]]:
-    ny = element-df.loc[df.index[4]]'''
+
 # Plot results
 plt.figure(figsize=(10, 6))
 plt.plot(t1, F, label='Forbruk [kr]')
+
+#plotter total SSB-forbruk (df.index[0]) minus alle forbruk som er "faste" utgifter.
 plt.plot(df.columns.astype(int), df.loc[df.index[0]]-df.loc[df.index[1]]-df.loc[df.index[6]]-df.loc[df.index[7]]-df.loc[df.index[8]]-df.loc[df.index[4]]-df.loc[df.index[10]], marker='o', label=df.index[0])
 plt.title('Simulering av forbrukervaner utgifter over tid')
 plt.xlabel('Tid (år)')
