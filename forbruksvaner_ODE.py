138acf46059cb8e9ccd722c8f60b218c0f79b3e8
MaggeSN <122047522+MaggeSN@users.noreply.github.com>
2023-11-18 13:19:23 +0100

    lagt til ODE for lån

--- forbruksvaner_ODE.py
+++ forbruksvaner_ODE.py
@@ -12,40 +12,45 @@ beta = 0.4      # Adjustment speed of spending habits (MPC)
 #g = 0.02        # Growth rate of income (Må ha inflasjon i modell for å kunne bruke denne. Antar i denne modellen at inflasjonen nuller ut økning i lønn.)
 U = 210000       # Fixed expenses (Ca. Gj. i norge mellom 1999-2012 i følge forbrukerundersøkelsen SSB) (Antar at denne også er konstant)
 P = 68000       # Payment amount for loans (5,6k i mnd)
-r = 0.02        # Interest rate on loans (Modellen antar fast rentesats)
+r = 0.03        # Interest rate on loans (Modellen antar fast rentesats)
 L0 = 960200     # Initial loan amount (338% av årslønnen som er gj. snitt ifølge Gjensidige)
 A0 = 284100    # Initial income (Gj. nettolønn i Norge er 402 000kr - 284 100kr i 2012)
 F0 = 105000      # Initial discretionary spending (Dette var gj. snitt i 1999 i følge dataen til SSB)
 
 # ODE system
-def dFdt(t, F, alpha, beta, U, P, r, L0, A0):
+def system(t, y, alpha, beta, U, P, r):
     #A = A0 * np.exp(g * t)       # Income growing exponentially
-    L = L0 * np.exp(-r * t) - P * t  # Loan decreasing over time
+    #L = L0 * np.exp(-r * t) - P * t  # Loan decreasing over time
+    F, L = y
+    dL = -P + r * L
     dF = alpha * (A0 - U - r * L) - beta * F
-    return dF
+    return [dF, dL]
+
 
 years = np.arange(1999,2013)
 t_span = (0, years[-1] - years[0])  # Time span for solve_ivp
 t_eval = years - years[0]  # Specific time points for evaluation
 
 # Solve ODE using solve_ivp
-result = solve_ivp(dFdt, t_span, [F0], args=(alpha, beta, U, P, r, L0, A0), t_eval=t_eval)
+result = solve_ivp(system, t_span, [F0, L0], args=(alpha, beta, U, P, r), t_eval=t_eval)
 
 # Extracting the solution
-F_solution = result.y[0]
+forbruk, lån = result.y
 
 # Time vector for plotting (1999 to 2012)
 
 
 # Plot results
 plt.figure(figsize=(10, 6))
-plt.plot(years, F_solution, label='Forbruk [kr]')
+plt.plot(years, forbruk, label='Forbruk [kr]')
+plt.plot(years, lån, label="lån")
 
 # Plotting additional data as in your original code
 plt.plot(df.columns.astype(int), df.loc[df.index[0]]-df.loc[df.index[1]]-df.loc[df.index[7]]-df.loc[df.index[8]]-df.loc[df.index[4]]-df.loc[df.index[10]], marker='o', label=df.index[0])
-plt.title('Simulering av forbrukervaner utgifter over tid')
+plt.title('Simulering av forbrukervaner')
 plt.xlabel('Tid (år)')
 plt.ylabel('Forbruk kostnader')
+#plt.yscale('log')
 plt.legend()
 plt.grid(True)
 plt.show()
